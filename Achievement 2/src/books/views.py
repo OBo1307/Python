874b5fd44994bd8026551084577ce5c25849b291
OBo1307 <och.bogdan@gmail.com>
2023-12-18 09:40:34 +0000

    Exercise2.6
    ●	Create authentication for your web application
    ●	Use GET and POST methods
    ●	Password protect your web application’s views

--- Achievement 2/src/books/views.py
+++ Achievement 2/src/books/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView # to display lists and details
 from .models import Book                              # to access the Book model
+from django.contrib.auth.mixins import LoginRequiredMixin #to protect class-based view
 
 # Create your views here.
-class BookListView(ListView):                         # ListView is a class-based view
+class BookListView(LoginRequiredMixin, ListView):                         # ListView is a class-based "protected" view
     model = Book                                      # specify model
     template_name = 'books/main.html'                 # specify template
 
-class BookDetailView(DetailView):                     # DetailView is a class-based view
+class BookDetailView(LoginRequiredMixin, DetailView):                     # DetailView is a class-based "protected" view
     model = Book                                      # specify model
     template_name = 'books/detail.html'               # specify template
