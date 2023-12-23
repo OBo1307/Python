34590e9d7b24ee9e9f559403ac6882381f6a80f4
OBo1307 <och.bogdan@gmail.com>
2023-12-23 17:06:33 +0000

    Exercise2.7
    ●	Work on elements of two-way communication like creating forms and buttons
    ●	Implement search and visualization (reports/charts) features
    ●	Use QuerySet API, DataFrames (with pandas), and plotting libraries (with matplotlib)

--- Achievement 2/src/sales/models.py
+++ Achievement 2/src/sales/models.py
@@ -6,7 +6,7 @@ class Sale(models.Model):
     book= models.ForeignKey(Book, on_delete=models.CASCADE)
     quantity= models.PositiveIntegerField()
     price= models.FloatField()
-    data_create= models.DateTimeField(auto_now_add=True)
+    date_created= models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"id: {self.id}, book: {self.book.name}, quantity: {self.quantity}"
